5161fa446dd843c37f3776c48c0d8a953b7e820c
Balázs Orbán <285574+orbanbalage@users.noreply.github.com>
2025-12-05 13:23:29 +0100

    add list to numpy array function

--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,7 @@
 # YOLO
 
+import numpy
+
 def file_to_list(file_path, separator, pop_final):
 	with open(file_path) as input_file:
 		input_text = input_file.read()
@@ -7,3 +9,16 @@ def file_to_list(file_path, separator, pop_final):
 	if pop_final:
 		input_list.pop()
 	return input_list
+
+def list_to_numarray(input_list):
+	# rows = len(input_list)
+	# cols = len(input_list[0])
+
+	# num_array = numpy.empty([rows,cols], dtype="U")
+
+	# faster and cheaper to dynamically build the python array and convert the final product to numpy
+	arr = []
+	for i in input_list:
+		arr.append(list(str(i)))
+	
+	return numpy.array(arr)
