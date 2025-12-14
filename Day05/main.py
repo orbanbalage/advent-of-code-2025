f676bded941af816094f498f1b9231a9f9d93f6d
Balázs Orbán <285574+orbanbalage@users.noreply.github.com>
2025-12-14 23:53:24 +0100

    second part working whoohoo

--- Day05/main.py
+++ Day05/main.py
@@ -4,13 +4,17 @@ sys.path.append("../utils/")
 import collections
 from functions import file_to_list
 from functions import arr_to_arrs
+from functions import range_overlap
+from functions import merge_range
+from functions import ranges_overlap
+from functions import ranges_merge
 
 import numpy
 
 input_sample_list = file_to_list("input-sample", "\n", pop_final=True)
 input_list = file_to_list("input.txt", "\n", pop_final=True)
 
-result = []
+result_1 = []
 
 # arrs = arr_to_arrs(input_sample_list, "")
 arrs = arr_to_arrs(input_list, "")
@@ -25,9 +29,16 @@ for ID in IDs:
 	for r in ranges:
 		if ID in r:
 			# print(ID, r)
-			result.append(ID)
+			result_1.append(ID)
 
+# inplace sort
+ranges.sort(key=lambda a : a.start)
 
-print(len(set(result)))
+while ranges_overlap(ranges):
+	# print("overlap found, ranges length =" + str(len(ranges)))
+	ranges = ranges_merge(ranges)
+else:
+	# print("calculating result, ranges length =", str(len(ranges)))
+	print(sum([len(x) for x in ranges]))
 
-
+print(len(set(result_1)))
